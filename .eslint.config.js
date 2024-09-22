638b92eb1a55afb9d6fd79b4b821975fea18f4b3
tomasz13nocon <tomasz13nocon@gmail.com>
2024-09-22 12:13:46 +0200

    migrate eslint

--- .eslint.config.js
+++ .eslint.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   root: true,
   parser: "@typescript-eslint/parser",
   extends: [
@@ -8,7 +8,20 @@ module.exports = {
     "plugin:svelte/recommended",
   ],
   plugins: ["@typescript-eslint"],
-  ignorePatterns: ["*.cjs"],
+  ignorePatterns: [
+    "*.cjs",
+    ".DS_Store",
+    "node_modules",
+    "/build",
+    "/.svelte-kit",
+    "/package",
+    ".env",
+    ".env.*",
+    "!.env.example",
+    "pnpm-lock.yaml",
+    "package-lock.json",
+    "yarn.lock",
+  ],
   rules: {
     "@typescript-eslint/no-non-null-assertion": "off",
     "@typescript-eslint/ban-ts-comment": "off",
